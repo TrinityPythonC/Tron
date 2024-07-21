771c91805cbcd7eb79716f918c3c0f83057e19a9
TrinityPythonC <155504630+TrinityPythonC@users.noreply.github.com>
2024-07-22 09:06:44 +1000

    Add files via upload

--- tron.py
+++ tron.py
@@ -5,9 +5,7 @@ mainwin = Tk()
 mainwin.geometry("800x600")
 
 canvas1= Canvas(mainwin,width=800,height=600, bg = "black")
-
 canvas1.place(x=0,y=0)
-
 
 player1alive = True
 x1 = 50 # player 1 x-location
@@ -21,15 +19,12 @@ y2 = 50 # player 2 y-location
 dx2 = 0 # x2 speed
 dy2 = 1 # y2 speed
 
-
-
 grid = []  # playfield, 500 by 500
 for i in range(500):
     L=[]
     for j in range(500):
         L.append(0)
     grid.append(L)
-
 
 def mykey(event):
     global dx1, dy1, dx2, dy2
@@ -58,19 +53,18 @@ def mykey(event):
         dy2 = 1
         dx2 = 0
 
-
 def drawdot(x,y,colour):
     global grid
     grid[x][y] = 1
     canvas1.create_line(x*4,y*4,x*4+4,y*4,width=4,fill=colour)
 
-
 def drawline(x,y,dx,dy,n,colour):
     for i in range(n):
         drawdot(x+dx*i,y+dy*i,colour)
     
-
-mainwin.bind("<Key>", mykey)
+def drawline(x,y,dx,dy,n,colour):
+    for i in range(n):
+        drawdot(x+dx*i,y+dy*i,colour)
 
 def timerupdate():
     global x1,x2,y1,y2, player1alive, player2alive
@@ -80,7 +74,7 @@ def timerupdate():
     if player2alive:
       x2 = x2 + dx2
       y2 = y2 + dy2
-    if grid[x1][y1] == 1:
+    if grid[x1][y1] == 1:  # collision
        player1alive = False
     if grid[x2][y2] == 1:
        player2alive = False
@@ -88,13 +82,12 @@ def timerupdate():
     drawdot(x2,y2,"red")
     mainwin.after(100,timerupdate)
 
-
 # drawwalls
 drawline(0,1,1,0,200,"yellow")
 drawline(199,1,0,1,150,"yellow")
 drawline(199,150,-1,0,200,"yellow")
 drawline(0,150,0,-1,150,"yellow")
 
-
+mainwin.bind("<Key>", mykey)
 mainwin.after(100,timerupdate)
 mainwin.mainloop()
